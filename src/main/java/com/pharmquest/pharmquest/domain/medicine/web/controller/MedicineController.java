ed6183191e161f3b1cca2087abb4dc48eecc9414
joamksh <joamksh@naver.com>
2025-01-20 16:04:11 +0900

    feat  : 단일 약 전체 정보 조회 (개발 확인용)

--- src/main/java/com/pharmquest/pharmquest/domain/medicine/web/controller/MedicineController.java
+++ src/main/java/com/pharmquest/pharmquest/domain/medicine/web/controller/MedicineController.java
@@ -1,5 +1,6 @@
 package com.pharmquest.pharmquest.domain.medicine.web.controller;
 
+import com.pharmquest.pharmquest.domain.medicine.repository.MedicineRepository;
 import com.pharmquest.pharmquest.domain.medicine.web.dto.MedicineResponseDTO;
 import com.pharmquest.pharmquest.domain.medicine.service.MedicineService;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,9 +15,11 @@ import java.util.List;
 public class MedicineController {
 
     private final MedicineService medicineService;
+    private final MedicineRepository medicineRepository;
 
-    public MedicineController(MedicineService medicineService) {
+    public MedicineController(MedicineService medicineService, MedicineRepository medicineRepository) {
         this.medicineService = medicineService;
+        this.medicineRepository = medicineRepository;
     }
 
     @GetMapping("/search")
@@ -24,6 +27,13 @@ public class MedicineController {
             @RequestParam(defaultValue="openfda.product_type:OTC") String query,
             @RequestParam(defaultValue = "10") int limit) {
         return medicineService.getMedicines(query, limit);
+    }
+
+    @GetMapping("/total")
+    public String viewTotal(
+            @RequestParam(defaultValue = "openfda.product_type:OTC") String query,
+            @RequestParam(defaultValue = "1") int limit) {
+        return medicineRepository.findTotal(query, limit);
     }
 }
 

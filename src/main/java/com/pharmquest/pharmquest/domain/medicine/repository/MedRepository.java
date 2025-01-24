23ec00d74fc00787d5d7b57e93b31523c1e98385
joamksh <joamksh@naver.com>
2025-01-25 00:49:11 +0900

    ref : medRepository 수정

--- src/main/java/com/pharmquest/pharmquest/domain/medicine/repository/MedRepository.java
+++
@@ -1,7 +0,0 @@
-package com.pharmquest.pharmquest.domain.medicine.repository;
-
-import com.pharmquest.pharmquest.domain.medicine.data.Medicine;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MedRepository extends JpaRepository<Medicine, Long> {
-}

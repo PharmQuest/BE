72e4ee02624dc27c23dd460c529ac28b17121658
joamksh <joamksh@naver.com>
2025-01-25 10:44:59 +0900

    ref : json 파싱 예외 처리

--- src/main/java/com/pharmquest/pharmquest/domain/medicine/service/TranslationService.java
+++ src/main/java/com/pharmquest/pharmquest/domain/medicine/service/TranslationService.java
@@ -4,6 +4,8 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.cloud.translate.Translate;
 import com.google.cloud.translate.TranslateOptions;
 import com.google.cloud.translate.Translation;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -23,6 +25,9 @@ public class TranslationService {
             throw new IllegalArgumentException("Google Cloud JSON Key가 비어 있습니다!");
         }
 
+        // JSON 유효성 검증
+        validateJson(jsonKey);
+
         // JSON 문자열로 GoogleCredentials 생성
         GoogleCredentials credentials = GoogleCredentials.fromStream(
                 new ByteArrayInputStream(jsonKey.getBytes())
@@ -35,10 +40,25 @@ public class TranslationService {
     }
 
     public String translateText(String text, String targetLanguage) {
-        Translation translation = translate.translate(
+        return this.translate.translate(
                 text,
                 Translate.TranslateOption.targetLanguage(targetLanguage)
-        );
-        return translation.getTranslatedText();
+        ).getTranslatedText();
+    }
+
+    /**
+     * JSON 유효성을 검증하는 메서드
+     *
+     * @param jsonKey JSON 문자열
+     */
+    private void validateJson(String jsonKey) {
+        try {
+            JsonElement jsonElement = JsonParser.parseString(jsonKey);
+            if (!jsonElement.isJsonObject()) {
+                throw new IllegalArgumentException("JSON Key가 유효한 객체 형식이 아닙니다!");
+            }
+        } catch (Exception e) {
+            throw new IllegalArgumentException("JSON Key가 유효하지 않습니다!", e);
+        }
     }
 }

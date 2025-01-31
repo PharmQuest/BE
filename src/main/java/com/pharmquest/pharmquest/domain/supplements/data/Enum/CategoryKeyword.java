0b6f9c44764bf3b0d9a06a1ad556f57453e33a7f
김정훈\kindd <kinddevil02@gmail.com>
2025-01-31 13:53:43 +0900

    [Refactor] 한국 데이터 직접 추가로 인한 카테고리 및 세부사항 수정

--- src/main/java/com/pharmquest/pharmquest/domain/supplements/data/Enum/CategoryKeyword.java
+++ src/main/java/com/pharmquest/pharmquest/domain/supplements/data/Enum/CategoryKeyword.java
@@ -2,5 +2,6 @@ package com.pharmquest.pharmquest.domain.supplements.data.Enum;
 
 public enum CategoryKeyword {
     전체, 눈건강, 자외선, 피부, 미네랄, 간건강, 관절, 속쓰림,
-    소화, 허리, 피로, 발진, 두피, 칼슘, 염좌
+    소화, 허리, 피로, 발진, 두피, 칼슘, 염좌, 항산화, 기관지,
+    유산균, 장건강, 체력
 }

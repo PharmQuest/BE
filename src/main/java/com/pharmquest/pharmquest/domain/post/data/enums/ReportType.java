7c79fc3a4856fdba3c09e34f1f21ef13b1f2bba7
heessunny <kimheesun107@gmail.com>
2025-01-31 23:49:44 +0900

    refactor: 신고하기 기능 Enum에 한국어 정보 추가

--- src/main/java/com/pharmquest/pharmquest/domain/post/data/enums/ReportType.java
+++ src/main/java/com/pharmquest/pharmquest/domain/post/data/enums/ReportType.java
@@ -2,10 +2,33 @@ package com.pharmquest.pharmquest.domain.post.data.enums;
 
 import com.pharmquest.pharmquest.global.apiPayload.code.status.ErrorStatus;
 import com.pharmquest.pharmquest.global.apiPayload.exception.handler.CommonExceptionHandler;
+import lombok.Getter;
 
 import java.util.Arrays;
 
+@Getter
 public enum ReportType {
-    ABUSIVE_LANGUAGE, OBSCENE_CONTENT, DEFAMATION, PERSONAL_INFO, OTHER
 
-}
+    SPAM("스팸 홍보/도배"),
+    OBSCENE_CONTENT("음란물"),
+    ILLEGAL_INFO("불법 정보 포함"),
+    PROFANITY("욕설"),
+    HATE_SPEECH("비하/혐오/차별적 표현"),
+    FRAUD("유출/사칭/사기"),
+    ILLEGAL_RECORDINGS("불법 촬영물 유통"),
+    ADS("상업적 광고 및 판매");
+
+    private final String koreanName;
+
+    ReportType(String name) {
+        this.koreanName = name;
+    }
+
+    public static ReportType fromKoreanName(String koreanName) {
+        return Arrays.stream(values())
+                .filter(type -> type.getKoreanName().equals(koreanName))
+                .findFirst()
+                .orElseThrow(() -> new CommonExceptionHandler(ErrorStatus.ILLEGAL_POST_CATEGORY));
+    }
+
+    }

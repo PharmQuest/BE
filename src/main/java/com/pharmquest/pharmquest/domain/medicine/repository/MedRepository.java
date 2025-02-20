f9563ab9fff555c90c726d2fe35bee18408f1b62
joamksh <joamksh@naver.com>
2025-02-20 13:00:09 +0900

    ref : 필터링 값 인식 변경

--- src/main/java/com/pharmquest/pharmquest/domain/medicine/repository/MedRepository.java
+++ src/main/java/com/pharmquest/pharmquest/domain/medicine/repository/MedRepository.java
@@ -38,10 +38,10 @@ public interface MedRepository extends JpaRepository<Medicine, Long> {
     Page<Medicine> findByCategoryAndCountry(MedicineCategory category, String country, Pageable pageable);
 
     //  카테고리 + 키워드 + 국가 필터 추가
-    @Query("SELECT m FROM Medicine m WHERE LOWER(m.category) = LOWER(:category) " +
+    @Query("SELECT m FROM Medicine m WHERE m.category = :category " +
             "AND (:country = 'ALL' OR LOWER(m.country) = LOWER(:country)) " +
             "AND (LOWER(m.brandName) LIKE LOWER(CONCAT('%', :keyword, '%')) " +
-            "OR LOWER(m.indicationsAndUsage) LIKE LOWER(CONCAT('%', :keyword, '%')) " +
-            "OR LOWER(m.category) LIKE LOWER(CONCAT('%', :keyword, '%'))) ")
+            "OR LOWER(m.indicationsAndUsage) LIKE LOWER(CONCAT('%', :keyword, '%')))")
     Page<Medicine> findByCategoryKeywordAndCountry(MedicineCategory category, String keyword, String country, Pageable pageable);
+
 }
